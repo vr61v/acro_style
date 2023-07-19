8303713327d5dd1f7a07b4e3e9af9b7d767d9188
Vr61v <nikita20040503@gmail.com>
2023-07-19 20:32:24 +0300

    Create teachers page

---
+++ js/cards.js
@@ -0,0 +1,9 @@
+VanillaTilt.init(document.querySelectorAll(".teachers__list-item"), {
+  reverse: true,
+  transition: true, 
+  easing: "cubic-bezier(.03,.98,.52,.99)",
+  scale: 1.05,
+  speed: 1000,
+  max: 5,
+});
+	
