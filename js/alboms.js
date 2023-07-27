4866900c67450aea362fe68a0c6b6e18bfef16dd
Vr61v <nikita20040503@gmail.com>
2023-07-27 23:21:56 +0300

    Update Gallery

--- js/alboms.js
+++ js/alboms.js
@@ -1,15 +1,15 @@
 var object = [{
   "name": "АRENA STAGE",
   "date": "18.03.23",
-  "count": 11
+  "count": 360
 },{
   "name": "Русские Звезды",
   "date": "28.01.23",
-  "count": 9
+  "count": 21
 },{
   "name": "Турнир Да.Бро",
   "date": "25.03.23",
-  "count": 7
+  "count": 108
 }]
 
 var data = JSON.parse(JSON.stringify(object));
@@ -37,7 +37,73 @@ function drawData(active){
       </div>`;
   } 
 }
+
+function pagination(element) {
+  let page = 1;
+  const items = 12;
+
+  function draw(countItems, currentPage){
+    var list = document.getElementById("gallery-content");
+    list.innerHTML = "";
+    --currentPage;
+
+    for(let i = countItems * currentPage + 1; i <= countItems * currentPage + countItems; ++i){
+      const image = new Image();
+      image.src = `../source/alboms/${data[element]['name']}/Файл ${i}.jpg`;
+      if(image.width < image.height){
+        list.innerHTML += `<div class="gallery__img-lg"> <img class="gallery__img" src="../source/alboms/${data[element]['name']}/Файл ${i}.jpg" /> </div>`;
+        continue;
+      }
+      list.innerHTML += `<div class="gallery__img-sm"> <img class="gallery__img" src="../source/alboms/${data[element]['name']}/Файл ${i}.jpg" /> </div>`;
+    }
+  }
+  function display(currpage, countItems) {
+    const buttons = document.querySelector('.gallery__buttons');
+    buttons.innerHTML = ""
+    const pages = Math.ceil(data[element]['count'] / countItems);
+    const ul = document.createElement("ul");
+    ul.classList.add('gallery__buttons--ul');
+
+    for (let i = 0; i < pages; ++i) {
+      const li = displayButtons(i + 1);
+      ul.appendChild(li);
+    }
+    buttons.appendChild(ul)
+  }
+  function displayButtons(pageCurr){
+    const li = document.createElement("li");
+    li.innerText = pageCurr;
+
+    if(page == pageCurr){
+      li.classList.add("gallery__active");
+    }
+
+    li.addEventListener('click', () => {
+      page = pageCurr;
+      draw(items, page);
+      document.querySelector("li.gallery__active").classList.remove("gallery__active");
+      li.classList.add('gallery__active');
+    })
+
+    return li;
+  }
+
+  draw(items, page);
+  display(page, items);
+}
+
 drawData(-1);
+function openGallery(element){
+  document.getElementById("gallery").style.display = "block";
+  document.getElementById("body").style.overflow = "hidden";
+  document.getElementsByClassName("gallery__data")[0].innerText = `${data[element]['name']}, ${data[element]['date']}`;
+
+  pagination(element);
+}
+function closeGallery(){
+  document.getElementById("gallery").style.display = "none";
+  document.getElementById("body").style.overflow = "auto";
+}
 
 document.getElementById('SortBy1').addEventListener('click', e =>{
   drawData(1);
@@ -63,29 +129,12 @@ document.getElementById('SortBy6').addEventListener('click', e =>{
   drawData(6);
   document.getElementById('SortByActive').innerHTML = 'Количество фото <';
 })
-
-
-
-function openGallery(element){
-  document.getElementById("gallery").style.display = "block";
-  document.getElementById("body").style.overflow = "hidden";
-  
-  var gallery = document.getElementById("gallery-content");
-  gallery.innerHTML = "";
-  for(let i = 1; i <= data[element]['count']; ++i){
-    gallery.innerHTML += `<div> <img class="gallery__img" src="../source/alboms/${data[element]['name']}/Файл ${i}.jpg" /> </div>`;
-  }
-  document.getElementsByClassName('photo__gallery').innerHTML = gallery;
-  
-}
-function closeGallery(){
-  document.getElementById("gallery").style.display = "none";
-  document.getElementById("body").style.overflow = "auto";
-}
-
 window.onclick = function(event) {
   if (event.target == document.getElementById("gallery")) {
     document.getElementById("gallery").style.display = "none";
     document.getElementById("body").style.overflow = "auto";
   }
 }
+
+
+
